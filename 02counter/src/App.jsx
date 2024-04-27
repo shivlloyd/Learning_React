6338f3d5eaf1bb697b44be2b8ed5a07106f29060
Shivam Kumar <shivamkumar.2121997@gmail.com>
2024-04-27 16:31:58 +0530

    A react interview question on counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   const addValue = () => {
     if (counter < 20) {
-      setCounter(counter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
     }
   };
 
