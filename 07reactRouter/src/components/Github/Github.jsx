d816aff53c8d5bf28cff3246f31afd1e4919363e
Shivam Kumar <shivamkumar.2121997@gmail.com>
2024-05-02 11:47:47 +0530

    React router crash course

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/shivlloyd")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/shivlloyd")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
@@ -20,3 +22,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/shivlloyd");
+  return response.json();
+};
