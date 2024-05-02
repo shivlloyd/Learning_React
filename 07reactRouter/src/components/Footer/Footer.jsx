d2065e3e93515c3619284379593d4be59de9f7f2
Shivam Kumar <shivamkumar.2121997@gmail.com>
2024-05-02 15:20:46 +0530

    Update Footer.jsx

--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/shivlloyd"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -77,9 +77,12 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
-            © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            © 2024
+            <a
+              href="https://shivam-kumar-site.netlify.app/"
+              className="hover:underline"
+            >
+              shivlloyd
             </a>
             . All Rights Reserved.
           </span>
