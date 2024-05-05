9a133d081e3b085ac3710c64d5ce10c5ce68be9e
Shivam Kumar <shivamkumar.2121997@gmail.com>
2024-05-05 20:52:09 +0530

    context API

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import UserContextProvider from "./context/UserContextProvider";
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Chai aur contextAPI</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
